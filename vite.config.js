77ae63c741de8c9ffebef43332ae453fcf7f675c
Kirill <k.godov@list.ru>
2024-12-20 18:10:02 +0500

    initial-commit-1

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/New_site_UGLTU/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
